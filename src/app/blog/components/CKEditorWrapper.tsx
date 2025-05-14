d07aa69a2969eb7315290f59e493a31617e5f505
Lucas Calviño <calvino.lucas@gmail.com>
2025-05-14 06:59:53 -0300

    refactor: fixed CKEditor props - Renamed value prop to initialData in BlogComponent - Updated error messages to be more specific

--- src/app/blog/components/CKEditorWrapper.tsx
+++ src/app/blog/components/CKEditorWrapper.tsx
@@ -1,40 +1,53 @@
-import React from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
+import React, { useEffect, useRef } from 'react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 import './CKEditorWrapper.scss';
 
+// Props we need for the editor
 interface CKEditorWrapperProps {
-  value: string;
-  onChange: (data: string) => void;
+    initialData: string;
+    onChange: (data: string) => void;
 }
 
-const CKEditorWrapper: React.FC<CKEditorWrapperProps> = ({ value, onChange }) => {
-  return (
-    <CKEditor
-      editor={ClassicEditor}
-      data={value}
-      config={{
-        licenseKey: 'GPL',
-        toolbar: {
-          items: [
-            'bold',
-            'italic',
-            'underline',
-            '|',
-            'bulletedList',
-            'numberedList',
-            '|',
-            'undo',
-            'redo'
-          ]
-        }
-      }}
-      onChange={(_event, editor) => {
-        const data = editor.getData();
-        onChange(data);
-      }}
-    />
-  );
-};
+export const CKEditorWrapper: React.FC<CKEditorWrapperProps> = ({ initialData, onChange }) => {
+    // Keep track of the editor instance
+    const editorRef = useRef<any>(null);
 
-export default CKEditorWrapper;
+    // Clean up when we're done
+    useEffect(() => {
+        return () => {
+            if (editorRef.current) {
+                editorRef.current.destroy();
+            }
+        };
+    }, []);
+
+    return (
+        <div style={{ height: '300px' }}>
+            <CKEditor
+                editor={ClassicEditor}
+                data={initialData}
+                config={{
+                    licenseKey: 'GPL',
+                    toolbar: [
+                        'heading',
+                        '|',
+                        'bold',
+                        'italic',
+                        'underline',
+                        '|',
+                        'bulletedList',
+                        'numberedList'
+                    ]
+                }}
+                onReady={editor => {
+                    editorRef.current = editor;
+                }}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    onChange(data);
+                }}
+            />
+        </div>
+    );
+};
